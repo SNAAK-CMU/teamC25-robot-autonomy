cd72fe1ed68c50871d7f2c0e19944aec8145dcc8
AbhinandanVellanki <abhinandan.vellanki@gmail.com>
2025-04-01 15:21:50 -0400

    added gitignore

---
+++ save_image_realsense.py
@@ -0,0 +1,33 @@
+# subscribe to realsense camera and save images
+
+import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import yaml
+import tf.transformations as tf_transformations
+from autolab_core import RigidTransform
+import time
+
+
+# Callback function for the realsense camera
+
+def callback(data):
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    cv2.imshow("Image window", cv_image)
+
+    # save the image to a file upon key press
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("image.jpg", cv_image)
+        print("Image saved!")
+
+# Initialize the node
+rospy.init_node('save_image_realsense', anonymous=True)
+
+# Subscribe to the realsense camera
+rospy.Subscriber("/camera/color/image_raw", Image, callback)
+
+# Keep the node running
+rospy.spin()
